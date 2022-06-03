a54b78e0ba7af1e9e0f3b5a2a803cff726b05b7e
seungchanKKK <eccsck@gmail.com>
2022-06-03 16:01:25 +0900

    스프링심화 2주차중간쯤

---
+++ springcore/src/main/java/com/sparta/springcore/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.springcore.repository;
+
+import com.sparta.springcore.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByUsername(String username);
+}
